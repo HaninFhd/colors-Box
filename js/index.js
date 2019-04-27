5a565e4b974788b3c5c5a22210cb88faaaf2d7bf
haninFhd <hanin.fhd@gmail.com>
2019-04-27 21:14:04 +0300

    firt commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+  // Your code goes here!
+  
+  }
+
+  function myFunction() {
+  let x = document.getElementById("mySubmit");
+  let table = document.getElementById("pixelCanvas");
+   let table_Height = document.getElementById("inputHeight");
+  let row = table.insertRow(table_Height);
+  let cell1 = row.insertCell(0);
+
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+  
